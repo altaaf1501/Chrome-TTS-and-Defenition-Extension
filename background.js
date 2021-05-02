aa862dd3c8f69c75e3be562f9e7036b32dfd5dae
altaaf1501 <ajahankeer@ryerson.ca>
2021-05-02 09:43:52 -0400

    Altaaf Seventh Commit - Added Thesaurus Function

--- background.js
+++ background.js
@@ -11,6 +11,9 @@ chrome.commands.onCommand.addListener(function(command) {
       case 'definition_Check':
           defFunc();
           break;
+      case 'synonym_Check':
+          synFunc();
+          break;
       default:
           console.log(`Command ${command} not found`);
   }
@@ -22,11 +25,21 @@ chrome.commands.onCommand.addListener(function(command) {
 * tts Function Code
 */
 function ttsFunc() {
-   const query = { active: true, currentWindow: true };
-   chrome.tabs.query(query, (tabs) => {
-       chrome.tabs.create({ url: tabs[0].url, active: false });
-   });
-  console.log("Running TTS Function.");
+
+  // chrome.tts.speak(text);
+
+  // var speakListener = function(utterance, options, sendTtsEvent) {
+  //   sendTtsEvent({type: 'start', charIndex: 0})
+  
+  //   sendTtsEvent({type: 'end', charIndex: text.length})
+  // };
+  
+  // const stopListener = () => {
+  //   // (stop all speech)
+  // };
+  
+  // chrome.ttsEngine.onSpeak.addListener(speakListener);
+  // chrome.ttsEngine.onStop.addListener(stopListener);
 }
 
 
@@ -37,7 +50,7 @@ function ttsFunc() {
 */
 function defFunc() {
   
-  console.log("Running Definition Function.");
+  console.log("Running Dictionary Function.");
   
   const selectedWord = text;
 
@@ -48,7 +61,6 @@ function defFunc() {
     let sdl = [];
     for (var i = 0; i < dil; i++) {
       sdl.push(defInfo[i].shortdef.length);
-      //console.log(sdl);
     }
     
     var j, k;
@@ -69,20 +81,122 @@ function defFunc() {
 
     } 
 
-    //console.log(defInfo);
-
   })
   .catch(error => console.error('Valid Word Not Highlighted.')); 
 
 }
 
 
+
+/**
+* word definition Function Code
+*/
+function synFunc() {
+  
+  console.log("Running Thesaurus Function.");
+  
+  const selectedWord = text;
+
+  fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${selectedWord}?key=252e4239-5b8b-4050-98c6-8e69d790d5d7`)
+  .then(response => response.json())
+  .then(defInfo => {
+
+    const dil = defInfo.length;
+
+    console.log(defInfo);
+
+    for (var i = 0; i < dil; i++) {
+
+      let synl = defInfo[i].meta.syns.length;
+      let antl = defInfo[i].meta.ants.length;
+      let rell = defInfo[i].def[0].sseq[0][0][1].rel_list.length;
+
+      //Count
+      console.log("Type " + i + ":");
+
+
+      //Synonyms
+      if (synl >= 1) {  
+        console.log(`     Synonyms of ${selectedWord}` + ` (` + defInfo[i].fl + `):`);
+        let synjl = [];
+
+        for (var z = 0; z < synl; z++) {
+          synjl.push(defInfo[i].meta.syns[z].length);
+        }
+
+        let count = 1;
+
+        for (var j = 0; j < synl; j++) {
+          for (var x = 0; x < synjl[j]; x++) {
+            console.log("          " + (count) + ". " + defInfo[i].meta.syns[j][x]);
+            count = count + 1;
+          }
+        }
+      } 
+      else if (synl <= 0) {
+        console.log(`     There are no synonyms for ${selectedWord}.`)
+      }
+
+      //Antonyms
+      if (antl >= 1) {  
+        console.log(`     Antonyms of ${selectedWord}` + ` (` + defInfo[i].fl + `):`);
+        let antjl = [];
+
+        for (var z = 0; z < antl; z++) {
+          antjl.push(defInfo[i].meta.ants[z].length);
+        }
+
+        let count = 1;
+
+        for (var j = 0; j < antl; j++) {
+          for (var x = 0; x < antjl[j]; x++) {
+            console.log("          " + (count) + ". " + defInfo[i].meta.ants[j][x]);
+            count = count + 1;
+          }
+        }
+
+      } 
+      else if (antl <= 0) {
+        console.log(`     There are no antonyms for ${selectedWord}.`)
+      }
+
+      
+
+      //Related Words
+      let rel2l = [];
+
+      if (rell >= 1) {
+        
+        console.log(`     Related words of ${selectedWord}` + ` (` + defInfo[i].fl + `):`)
+
+        let count = 1;
+
+        for (var j = 0; j < rell; j++) {
+          rel2l.push(defInfo[i].def[0].sseq[0][0][1].rel_list[j].length);
+          for (var k = 0; k < rel2l[j]; k++) {
+            console.log("          " + (count) + ": " + defInfo[i].def[0].sseq[0][0][1].rel_list[j][k].wd);
+            count = count + 1;
+          }
+        }
+      }
+      else if (rell <= 0) {
+        console.log(`     There are no related words of ${selectedWord}.`)
+      }
+    }
+
+  })
+  .catch(error => console.error('Word not found.')); 
+
+}
+
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log("Event recieved in background");
     text = request.txt;
-    defFunc();
-    chrome.tts.speak(txt);
+    //defFunc();
+    //chrome.tts.speak(text);
     sendResponse({success: true});
   }
 )
